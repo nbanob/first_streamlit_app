28e917380faad57211a1573e7f8c7ca866b7f16d
nbanob <73725691+nbanob@users.noreply.github.com>
2023-03-18 19:03:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-streamlit.title('Snowflake Streamlit App')
-streamlit.header('Breakfast Menu')
-streamlit.text('Oatmeal')
-streamlit.text('Smoothie')
-streamlit.text('Eggs')
+streamlit.title('My Parents New Healthy Diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
